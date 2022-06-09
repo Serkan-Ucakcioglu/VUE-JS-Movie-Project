44017aec7f75e9b9a7195fe9226065f14e2639c0
Serkan Uçakcıoğlu <serkanuckc123@gmail.com>
2022-06-09 04:30:21 +0300

    updated

--- src/components/DetailMovie.vue
+++ src/components/DetailMovie.vue
@@ -1,23 +1,21 @@
 <script setup>
 import HeaderBar from "./HeaderBar.vue";
-import { defineProps,ref} from "vue";
+import {defineProps,ref} from "vue";
 import axios from "axios";
 
 const props = defineProps(["id"]);
 const apiData = ref([]);
 const apiKey = ref("cd3758ae9695adf66bbf6bae68b8d777");
-const castData = ref([])
+const castData = ref([]);
 
-    axios
+axios
     .get(
         `https://api.themoviedb.org/3/movie/${props.id}?api_key=${apiKey.value}&append_to_response=similar_movies,credits,external_ids`
     )
     .then((res) => {
         apiData.value = res.data;
-        
-        castData.value = res.data.credits.cast.slice(0,15)
+        castData.value = res.data.credits.cast.slice(0, 15);
     });
-
 </script>
 
 <template>
@@ -27,15 +25,16 @@ const castData = ref([])
         <div class="movie">
             <div class="movie-poster">
                 <a :href="`https://image.tmdb.org/t/p/original${apiData.poster_path}`" target="_blank">
-                    <img  :src="`https://image.tmdb.org/t/p/w200${apiData.poster_path}`" />
+                    <img :src="`https://image.tmdb.org/t/p/w200${apiData.poster_path}`" />
                 </a>
 
                 <span class="movie-time">
                     {{ apiData.vote_average }} / 10 -- ⏱{{ apiData.runtime }} min
                 </span>
-
-                <div class="genres" v-for="genre in apiData.genres" :key="genre.id">
-                    <span>{{ genre.name }}</span>
+                <div class="genre">
+                    <div class="genres" v-for="genre in apiData.genres" :key="genre.id">
+                        <span>{{ genre.name }}</span>
+                    </div>
                 </div>
             </div>
             <div class="movie-details">
@@ -52,7 +51,7 @@ const castData = ref([])
     <h1>Actors</h1>
     <div class="container">
         <div class="cast-wrapper">
-            <div v-for="actor in  castData" :key="actor.id" class="cast">
+            <div v-for="actor in castData" :key="actor.id" class="cast">
                 <a :href="`https://image.tmdb.org/t/p/original/${actor.profile_path}`" :title="actor.name" :alt="actor.name" target="_blank">
                     <img v-if="actor.profile_path" :src="`https://image.tmdb.org/t/p/w92${actor.profile_path}`" :alt="actor.name" :title="actor.name" class="cast__image" loading="lazy" />
                 </a>
@@ -68,7 +67,7 @@ const castData = ref([])
 <style lang="scss" scoped>
 .hero {
     width: 100%;
-    min-height: 600px;
+    min-height: 500px;
     background-size: 100% 100%;
     background-repeat: no-repeat;
     display: flex;
@@ -127,19 +126,19 @@ const castData = ref([])
             display: flex;
             flex-direction: column;
             word-spacing: 5px;
-            line-height: 25px;
+            line-height: 20px;
             align-items: flex-start;
             justify-content: center;
             text-align: left;
             margin-left: 25px;
-            margin-top: 15px;
+            margin-top: 10px;
 
             h1 {
                 font-size: 35px;
                 line-height: 35px;
-                margin-bottom: 20px;
+                margin-bottom: 10px;
                 text-shadow: #fff 0 0 5px;
-                margin-right: 15px;
+               text-align: center;
             }
         }
     }
@@ -180,7 +179,7 @@ section {
                 margin-right: 10px;
                 margin-bottom: 15px;
 
-                &:hover{
+                &:hover {
                     background: #3fb883;
                 }
 
@@ -202,15 +201,34 @@ section {
 }
 
 @media only screen and (max-width: 600px) {
-    .hero{
-        min-height: 400px;
+    .hero {
+        min-height: 500px !important;
+        .genre {
+            display: flex;
+            justify-content: center;
+            .genres {
+                margin-right: 10px;
+            }
+        }
     }
-
     .movie {
         flex-direction: column;
+    }
+    .cast-wrapper {
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: flex-start !important;
+        text-align: center;
 
-        .movie-poster {
-            margin-top: 20px;
+        .cast{
+            border-radius: 15px !important;
+        }
+    }
+
+    section {
+        h1 {
+            text-align: left;
+            margin-left: 18px;
         }
     }
 }
