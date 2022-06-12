40c811d17955b50a430b53fa5e31c0ceff7e2b05
Serkan Uçakcıoğlu <serkanuckc123@gmail.com>
2022-06-13 02:01:45 +0300

    updated

--- src/App.vue
+++ src/App.vue
@@ -7,16 +7,17 @@
 
 <style lang="scss">
 * {
-  padding: 0;
-  margin: 0;
+    padding: 0;
+    margin: 0;
 }
 
 .container {
-width: 90%;
-max-width: 1200px;
-  margin: 0px auto;
+    width: 90%;
+    max-width: 1200px;
+    margin: 0px auto;
 }
-.actives{
-background: #3fb883;
+
+.actives {
+    background: #3fb883;
 }
 </style>
