d041372f4831490472e045f6239197fbdad9a77c
Serkan Uçakcıoğlu <serkanuckc123@gmail.com>
2022-06-13 16:58:35 +0300

    updated

--- src/components/SectionMovie.vue
+++ src/components/SectionMovie.vue
@@ -16,9 +16,7 @@ watch(
 axios
   .get(`https://api.themoviedb.org/3/${props.apiQuery}?api_key=${apiKey.value}`)
   .then((res) => {
-    apiData.value = res.data.results.filter(
-      (movie) => movie.media_type != "tv"
-    );
+    apiData.value = res.data.results
   });
 </script>
 <template>
@@ -26,14 +24,15 @@ axios
     <div class="container">
       <h1>{{ props.title }}</h1>
       <div class="movie-list">
-        <div class="movie-link" v-for="(data, index) in apiData" :key="data.id">
-          <router-link loading="lazy" :to="{ name: 'profile', params: { id: data.id } }">
+        <div loading="lazy" class="movie-link" v-for="(data, index) in apiData" :key="data.id">
+          <router-link  :to="{ name: 'profile', params: { id: data.id } }">
             <img
               :src="`https://image.tmdb.org/t/p/w200${data.poster_path}`"
               :alt="data.title || data.name"
               loading="lazy"
+              class="movie_img"
             />
-            <span class="head-title">{{ index + 1 }}. {{ data.title }} </span>
+            <span class="head-title">{{ index + 1 }}. {{ data.title || data.name}} </span>
           </router-link>
         </div>
       </div>
@@ -80,14 +79,15 @@ axios
     align-items: center;
 
     .movie-link {
+      display: block;
       position: relative;
       margin-right: 30px;
       width: 200px;
       height: 270px;
       cursor: pointer;
 
-      img {
-        height: 270px;
+      .movie_img {
+        height: 270px !important;
         box-shadow: -1px 0px 7px 8px rgba(0, 0, 0, 0.75);
       }
       &:hover {
