29c1298757e6ba941a8fc1872ec3f379e5dbe4b1
Serkan Uçakcıoğlu <serkanuckc123@gmail.com>
2022-06-16 02:52:13 +0300

    update

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,4 @@
 const { defineConfig } = require('@vue/cli-service')
-process.env.VUE_APP_VERSION = require('./package.json').version
 
 module.exports = defineConfig({
   transpileDependencies: true
